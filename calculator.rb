d318794054990196624db2e58ac3de30d6840a36
Tom Lynam <tom.s.lynam@gmail.com>
2016-05-11 14:58:23 -0600

    basic calculator that works

--- calculator.rb
+++ calculator.rb
@@ -3,15 +3,55 @@
 # make a commit
 # push to a github repo
 
-puts "Welcome to the ruby calculator"
-puts "Enter your first number:"
-# get the first number
-puts "Enter your operator"
-# get the operator
-puts "Enter the second number:"
-# get the second number
 
-# figure out how to do the math on the two numbers
-# output the result to the user
+operators = ["+", "-", "*", "/" ]
+
+puts "Welcome to the Ruby Calculator"
+
+# Get the first number
+puts "What is the first Number?"
+first_number = gets.chomp.to_f
+
+	if first_number.is_a?(Numeric)
+		else puts "'#{first_number}' is not a valid input, please try again."
+			exit
+	end
+
+
+# Get the operator
+puts "What is the operator?"
+modifier = gets.strip
+if operators.include?(modifier) == true
+else puts "Operator #{modifier} is not supported. I only support + - / * operators, please try again."
+	exit
+end
+
+
+# Get the second number
+puts "Enter the second number:"
+second_number = gets.chomp.to_f
+
+	if second_number.is_a?(Numeric)
+		else puts "'#{second_number}' is not a valid input, please try again."
+			exit
+	end
+
+
+
+# Calculate the answer
+puts "Calculating..."
+result = first_number.method(modifier).(second_number)
+
+
+
+# Output the result to the user
+puts "The Result of #{first_number} #{modifier} #{second_number} is #{result}."
+
+
 
 # Do all the bonus problems
+
+
+
+
+
